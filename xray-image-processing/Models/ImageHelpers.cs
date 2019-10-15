10b383b23175b56025adad7e13257c6a25fc2c4e
Hanna Grodzicka <hania.grodzicka@gmail.com>
2019-10-15 23:27:11 +0200

    Add inverting color method

---
+++ xray-image-processing/Models/ImageHelpers.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace XRayImageProcessing.Models
+{
+    public static class ImageHelpers
+    {
+        public static BitmapImage ToBitmapImage(this WriteableBitmap wbm)
+        {
+            BitmapImage bitmapImage = new BitmapImage();
+            using (MemoryStream stream = new MemoryStream())
+            {
+                PngBitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(wbm));
+                encoder.Save(stream);
+                bitmapImage.BeginInit();
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.StreamSource = stream;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+            }
+            return bitmapImage;
+        }
+    }
+}
--- xray-image-processing/Models/ImageProcessing.cs
+++ xray-image-processing/Models/ImageProcessing.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace XRayImageProcessing.Models
 {
@@ -12,15 +16,42 @@ namespace XRayImageProcessing.Models
             get { return _xRayBefore; }
             set { _xRayBefore = value; }
         }
+
         public XRayImage XRayAfter
         {
             get { return _xRayAfter; }
             set { _xRayAfter = value; }
         }
+
         public ImageProcessor(Uri uri)
         {
             _xRayBefore = new XRayImage(uri);
-            _xRayAfter = new XRayImage(uri);
+            _xRayAfter = InvertColour(new XRayImage(uri));
+        }
+
+        private XRayImage InvertColour(XRayImage xRayImage)
+        {
+            BitmapImage originalImage = xRayImage.XRayBitmap;
+
+            BitmapSource bitmapSource = new FormatConvertedBitmap(originalImage, PixelFormats.Pbgra32, null, 0);
+            WriteableBitmap modifiedImage = new WriteableBitmap(bitmapSource);
+
+            int h = modifiedImage.PixelHeight;
+            int w = modifiedImage.PixelWidth;
+            int[] pixelData = new int[w * h];
+            int widthInByte = 4 * w;
+
+            modifiedImage.CopyPixels(pixelData, widthInByte, 0);
+
+            for (int i = 0; i < pixelData.Length; i++)
+            {
+                pixelData[i] ^= 0x00ffffff;
+            }
+
+            modifiedImage.WritePixels(new Int32Rect(0, 0, w, h), pixelData, widthInByte, 0);
+
+            xRayImage.XRayBitmap = modifiedImage.ToBitmapImage();
+            return xRayImage;
         }
     }
 }

a8a9774cf3770105cd216fdd37d6a12355780243
Hanna Grodzicka <hania.grodzicka@gmail.com>
2019-11-05 22:42:41 +0100

    Refactor and improve coloring border

--- xray-image-processing/Models/Procesors/BorderFiller.cs
+++ xray-image-processing/Models/Procesors/BorderFiller.cs
@@ -1,28 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace XRayImageProcessing.Models.Procesors
 {
     class BorderFiller : IProcesor
     {
-        private int _delta = 100;
+        private readonly int _delta = 100;
+
+        private readonly int _percent = 25;
+
         public void process(int[] data, int width, int height)
         {
-            int[] borders = new int[2 * (_delta * width) + 2 * (_delta * (height - 2*_delta))];
+            int[] borders = new int[data.Length];
 
+            IterateBitmap(height, width, (h, w) =>
+            {
+                if (IsBorder(h, w, width, height))
+                {
+                    borders[(h * width) + w] = data[(h * width) + w];
+                }
+            });
+
+            Array.Sort(borders);
+            int limit = borders[_percent * (2 * (_delta * width) + 2 * (_delta * (height - 2 * _delta))) / 100];
+
+            IterateBitmap(height, width, (h, w) =>
+            {
+                if (data[(h * width) + w] < limit && IsBorder(h, w, width, height))
+                {
+                    data[(h * width) + w] = Color.Black.ToArgb();
+                }
+            });
+        }
+
+        private bool IsBorder(int h, int w, int width, int height) =>
+                        (h < _delta || h > height - _delta ||
+                         w < _delta || w > width - _delta);
+
+        private void IterateBitmap(int height, int width, Action<int, int> condition)
+        {
             for (int h = 0; h < height; h++)
             {
                 for (int w = 0; w < width; w++)
                 {
-                    if (h < _delta || h > height - _delta || 
-                        w < _delta || w > width - _delta)
-                    {
-                        data[(h * width) + w] = Color.Blue.ToArgb();
-                    }
+                    condition(h, w);
                 }
             }
         }

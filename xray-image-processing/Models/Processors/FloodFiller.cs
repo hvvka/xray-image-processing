8da8060926503a008394e881280c213e68e69659
Hanna Grodzicka <hania.grodzicka@gmail.com>
2019-12-03 21:10:17 +0100

    Create FillRanges method

--- xray-image-processing/Models/Processors/FloodFiller.cs
+++ xray-image-processing/Models/Processors/FloodFiller.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 
 namespace XRayImageProcessing.Models.Processors
 {
@@ -11,17 +14,14 @@ namespace XRayImageProcessing.Models.Processors
         {
             var sortedData = new int[data.Length];
             data.CopyTo(sortedData, 0);
-
             Array.Sort(sortedData);
             var limit = sortedData[Percent * width * height / 100];
 
-            IterateBitmap(height, width, (h, w) =>
+            IList<Tuple<int, int>> ranges = new List<Tuple<int, int>>
             {
-                if (data[h * width + w] < limit)
-                {
-                    data[h * width + w] = Color.Transparent.ToArgb();
-                }
-            });
+                new Tuple<int, int>(Color.Black.ToArgb(), limit)
+            };
+            this.FillRanges(data, width, height, ranges, Color.Transparent.ToArgb());
 
             return data;
         }
@@ -35,6 +35,21 @@ namespace XRayImageProcessing.Models.Processors
                     condition(h, w);
                 }
             }
+        }
+
+        public int[] FillRanges(int[] data, int width, int height, IList<Tuple<int, int>> ranges, int replacement)
+        {
+            foreach (var (lowerBound, upperBound) in ranges)
+            {
+                IterateBitmap(height, width, (h, w) =>
+                {
+                    if (data[h * width + w] >= lowerBound && data[h * width + w] <= upperBound)
+                    {
+                        data[h * width + w] = replacement;
+                    }
+                });
+            }
+            return data;
         }
     }
 }
